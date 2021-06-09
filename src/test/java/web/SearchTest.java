4b2f3c93a3e4b38d988b9e2e566487c1affdae07
Ivan Widyan <ivan.widyan@gdn-commerce.com>
2021-06-09 21:08:47 +0700

    finish rest assured api parallel web & api remove facebook login test

--- src/test/java/web/SearchTest.java
+++ src/test/java/web/SearchTest.java
@@ -1,8 +1,8 @@
+package web;
+
 import driver.PageObject;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -20,41 +20,38 @@ public class SearchTest extends PageObject {
         driver.get("https://www.google.com/");
     }
 
-    @Test(priority = 0)
+    @Test(priority = 1)
     public void verifyTitle() {
         Assert.assertEquals(driver.getTitle(), "Google", "Title is not matched");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void verifyLogo() {
         WebElement logo = driver.findElement(By.xpath("//img[@alt='Google']"));
         Assert.assertTrue(logo.isDisplayed(), "Logo is not displayed");
     }
 
     @Test(
-            priority = 2,
+            priority = 3,
             dependsOnMethods = {"verifyTitle", "verifyLogo"},
             alwaysRun = true)
     @Parameters({"keyword"})
-    public void search(String keyword) {
-        WebElement searchBox = driver.findElement(By.xpath("//input[@class='lst tiah']"));
-        searchBox.sendKeys(keyword + "\n");
+    public void search(String keyword) throws Exception {
+        WebElement searchBox = driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+        searchBox.sendKeys(keyword);
+        searchBox.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
     }
 
     @Test(
-            priority = 3,
+            priority = 4,
             dependsOnMethods = "search")
     @Parameters({"keyword"})
     public void checkSearchResult(String keyword) {
-        Boolean verified;
         System.out.println("start");
 
-        // only one result
-        /*WebElement searchResult = driver.findElement(By.xpath("//div/div/a/h3/div"));
-        Boolean searchResultVerified = searchResult.getText().toLowerCase().contains(keyword);*/
-
-        // all search results
-        List<WebElement> searchResult = driver.findElements(By.xpath("//div/div/a/h3/div"));
+        List<WebElement> searchResult = driver.findElements(By.xpath("//div/div/div/a/h3"));
         searchResult.stream().forEach(l -> System.out.println(l.getText()));
         Boolean searchResultVerified = searchResult.stream().allMatch(
                 l -> l.getText().toLowerCase().contains(keyword));
